9862520f4c31940e59a3eaa02e6583f42ededf73
RinRavit <128680924+RinRavit@users.noreply.github.com>
2024-04-02 20:07:58 +0700

    tp08

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Http\Response;
+
+class HomeController extends Controller
+{
+    public function renderHome(){
+        return view('home');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,159 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/Homepage.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <section class="navigation">
+    <!-- nav category -->
+        <div class="nav_category">
+            <div class="txt_category">
+                <h2>Featured Categories</h2>
+            </div>
+            <div class="category">
+                <li>
+                    <a href="">All</a>
+                </li>
+                <li>
+                    <a href="">Milks & Dairies</a>
+                </li>
+                <li>
+                    <a href="">Coffes & Teas</a>
+                </li>
+                <li>
+                    <a href="">Pet Foods</a>
+                </li>
+                <li>
+                    <a href="">Meats</a>
+                </li>
+                <li>
+                    <a href="">Vegetables</a>
+                </li>
+                <li>
+                    <a href="">Fruits</a>
+                </li>
+            </div>
+        </div>
+    <!-- end of nav category -->
+    
+    <!-- catagory -->
+        <div class="box-category">
+            <div class="cake&milk">
+                <div>
+                    <img src="asset/image/burger.png" alt="">
+                </div>
+                <div>
+                    <h4>Cake & Milk</h4>
+                </div>
+                <div>
+                    <p>14 items</p>
+                </div>
+            </div>
+            <div class="peach">
+                <div>
+                    <img src="asset/image/peach.png" alt="">
+                </div>
+                <div>
+                    <h4>Peach</h4>
+                </div>
+                <div>
+                <p>17 items</p>
+                </div>
+            </div>
+            <div class="oganic_kiwi">
+                <div>
+                    <img src="asset/image/kiwi.png" alt="">
+                </div>
+                <div>
+                    <h4>Oganic Kiwi</h4>
+                </div>
+                <div>
+                <p>21 items</p>
+                </div>
+            </div>
+            <div class="red_apple">
+                <div>
+                    <img src="asset/image/apple.png" alt="">
+                </div>
+                <div>
+                    <h4>Red Apple</h4>
+                </div>
+                <div>
+                <p>68 items</p>
+                </div>
+            </div>
+            <div class="snack">
+                <div>
+                    <img src="asset/image/snack.png" alt="">
+                </div>
+                <div>
+                    <h4>Snack</h4>
+                </div>
+                <div>
+                <p>34 items</p>
+                </div>
+            </div>
+            <div class="black_plum">
+                <div>
+                    <img src="asset/image/blackplum.png" alt="">
+                </div>
+                <div>
+                    <h4>Black Plum</h4>
+                </div>
+                <div>
+                <p>25 items</p>
+                </div>
+            </div>
+            <div class="vegetable">
+                <div>
+                    <img style="width: 80px;" src="asset/image/vegetables.png" alt="">
+                </div>
+                <div style="display: flex;
+                flex-direction:column; ">
+                    <h4>Vegetable</h4>
+                    <p>65 items</p>
+                </div>
+                <!-- <div class="count_category">
+                <p>65 items</p>
+                </div> -->
+            </div>
+            <div class="headphone">
+                <div>
+                    <img src="asset/image/headphone.png" alt="">
+                </div>
+                <div>
+                    <h4>Headphone</h4>
+                </div>
+                <div>
+                <p>33 items</p>
+                </div>
+            </div>
+            <div class="cake&milk">
+                <div>
+                    <img src="asset/image/milk&cake.png" alt="">
+                </div>
+                <div>
+                    <h4>Cake & Milk</h4>
+                </div>
+                <div>
+                <p>54 items</p>
+                </div>
+            </div>
+            <div class="orange">
+                <div>
+                    <img src="asset/image/orange.png" alt="">
+                </div>
+                <div>
+                    <h4>Orange</h4>
+                </div>
+                <div>
+                <p>63 items</p>
+                </div>
+            </div>
+        </div>
+        <!-- end of category -->
+    </section>
+</body>
+</html>
